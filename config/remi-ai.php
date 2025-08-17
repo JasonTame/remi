a4c1b817e54243bb0e0dbbcf88e5b68babfb708d
Jason Tame <jasonmiketame2@gmail.com>
2025-08-17 14:21:06 +0200

    Fix google redirect uri

--- config/remi-ai.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'base_url' => env('REMI_AI_BASE_URL'),
-];
--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://fwd.host/http://remi.test/auth/callback',
+        'redirect' =>  env('GOOGLE_REDIRECT_URI'),
     ]
 
 ];
