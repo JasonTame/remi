1e1307859135f20a83a46e5affd0202b1524ae40
Jason Tame <jasonmiketame2@gmail.com>
2025-10-26 10:08:09 +0200

    Remove scheduling logic
    We don't want to run the scheduler on laravel cloud becuase it runs 24/7, which is unnecessarily expensive

--- routes/console.php
+++ routes/console.php
@@ -1,14 +1,1 @@
 <?php
-
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
-
-// Define the application's command schedule
-// Process scheduled notifications every hour
-// This will now handle both generating recommendations AND sending notifications
-// based on each user's individual notification preferences
-Schedule::command('notifications:process-scheduled')
-    ->hourly()
-    ->name('process-scheduled-notifications')
-    ->withoutOverlapping();
